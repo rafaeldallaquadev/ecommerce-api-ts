19c8cac61b6e0573e34018341b5f1886f5e97a2c
Rafael Grisante Dallaqua <rafaeldallaquadev@gmail.com>
2026-04-15 08:07:15 -0300

    fix database connection

--- db.js
+++ db.js
@@ -6,5 +6,6 @@ export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASS
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 })
